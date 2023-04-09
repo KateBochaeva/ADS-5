d6a29bf3483b5206b97cff0a5c0173fc5d1f231e
KateBochaeva <117112623+KateBochaeva@users.noreply.github.com>
2023-04-09 23:28:05 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,13 +2,100 @@
 #include <string>
 #include <map>
 #include "tstack.h"
-
+int getPrior(char b){
+    if (b=='(')
+        return 0;
+    if (b==')')
+        return 1;
+    if ((b=='+')||(b=='-'))
+        return 2;
+    if ((b=='*')||(b=='/')){
+        return 3;
+    } else {
+        return -1;
+    }
+}
 std::string infx2pstfx(std::string inf) {
-  // добавьте код
-  return std::string("");
+   TStack<char, 100> st1;
+    std::string res;
+    int k = 0;
+    for (char a : inf) {
+        bool flag = 1;
+        if (getPrior(a) == -1) {
+            res += a;
+            res += ' ';
+            flag = 0;
+        }
+        if (getPrior(a) == 0) {
+            st1.Push(a);
+            flag = 0;
+        }
+        if (getPrior(a) > getPrior(st1.get())) {
+            st1.Push(a);
+            flag = 0;
+        }
+        if (st1.isEmpty() && getPrior(a) != -1) {
+            st1.Push(a);
+            flag = 0;
+        }
+        if (flag && a != ')') {
+            while (getPrior(st1.get()) >getPrior(a)) {
+                res += st1.pop();
+                res += ' ';
+            }
+            st1.Push(a);
+        }
+        if (a == ')') {
+            while (st1.get() != '(') {
+                res += st1.pop();
+                res += ' ';
+            }
+            st1.pop();
+        }
+        if (k == inf.size() - 1) {
+            while (!st1.isEmpty()) {
+                res += st1.pop();
+                if (st1.GetTop() != -1)
+                    res += ' ';
+            }
+        }
+        ++k;
+    }
+    return res;
+}
+
 }
 
 int eval(std::string pref) {
-  // добавьте код
-  return 0;
+  TStack<int, 100> st2;
+    for (char a : pref) {
+        if (a == ' ') {
+            continue;
+        }
+        if (a == '+') {
+            int p = st2.pop();
+            p += st2.pop();
+            st2.Push(p);
+        }
+        if (a == '-') {
+            int p = st2.pop();
+            p = st2.pop() - p;
+            st2.Push(p);
+        }
+        if (a == '*') {
+            int p = st2.pop();
+            p *= st2.pop();
+            st2.Push(p);
+        }
+        if (a == '/') {
+            int p = st2.pop();
+            p = st2.pop() / p;
+            st2.Push(p);
+        }
+        if ((a - '0') > 0) {
+            int o = a - '0';
+            st2.Push(o);
+        }
+    }
+    return st2.get();
 }
